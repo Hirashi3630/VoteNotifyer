cc0a8d2f5d369217df905600d3c35b366578fa48
Hirashi3630 <vladayolo.info@gmail.com>
2020-06-22 15:34:34 +0200

    old code added #2

--- Settings.py
+++ Settings.py
@@ -1,26 +1,28 @@
-#import json
+import json
+import io
+import os
+
+class Settings:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+        interval = data['interval']
+        sfx_path = data['sfx-path']
 
 
-#class Settings:
-    #def __init__(self, file_path):
-        #self.file_path = file_path
+    def Load(self):
+        if os.path.isfile(self.file_path) and os.access(self.file_path, os.R_OK):
+            # checks if file exists
+            print("File exists and is readable")
+        else:
+            print("Either file is missing or is not readable, creating file...")
+            with io.open(os.path.join(self.file_path, 'Accounts.json'), 'w') as db_file:
+                db_file.write(json.dumps({}))
 
-        #interval = data['interval']
-        #sfx_path = data['sfx-path']
-
-
-    #def Load(self):
-        #if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
-        #    # checks if file exists
-        #    print("File exists and is readable")
-        #else:
-        #    print("Either file is missing or is not readable, creating file...")
-        #    with io.open(os.path.join(PATH, 'Accounts.json'), 'w') as db_file:
-        #        db_file.write(json.dumps({}))
-        #with open(self.file_path) as config_file:
-        #    data = json.load(config_file)
+        with open(self.file_path) as config_file:
+            data = json.load(config_file)
 
 
 
-#s = Settings('config.json')
-#s.Load()
+s = Settings('config.json')
+s.Load()
