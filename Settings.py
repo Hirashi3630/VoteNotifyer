f5040988ff593af04fdc0ed9ff1d358243b237cf
Hirashi3630 <vladayolo.info@gmail.com>
2020-06-22 15:45:45 +0200

    fix downloading github config.json if doesn't exist

--- Settings.py
+++ Settings.py
@@ -2,13 +2,16 @@ import json
 import io
 import os
 
+import requests
+
+
 class Settings:
     def __init__(self, file_path):
         self.file_path = file_path
 
-        interval = data['interval']
-        sfx_path = data['sfx-path']
-
+        data = self.Load()
+        self.interval = data['interval']
+        self.sfx_path = data['sfx-path']
 
     def Load(self):
         if os.path.isfile(self.file_path) and os.access(self.file_path, os.R_OK):
@@ -16,13 +19,21 @@ class Settings:
             print("File exists and is readable")
         else:
             print("Either file is missing or is not readable, creating file...")
-            with io.open(os.path.join(self.file_path, 'Accounts.json'), 'w') as db_file:
-                db_file.write(json.dumps({}))
+            with io.open(os.path.join(self.file_path, 'config.json'), 'w') as db_file:
+                db_file.write(self.GetDefaultConfig())
 
         with open(self.file_path) as config_file:
             data = json.load(config_file)
 
+        return data
+
+    @staticmethod
+    def GetDefaultConfig():
+        default_config_url = 'https://raw.githubusercontent.com/Hirashi3630/VoteNotifyer/master/config.json'
+        return requests.get(default_config_url).content.decode("utf-8")
 
 
 s = Settings('config.json')
-s.Load()
+print(s.interval)
+print(s.sfx_path)
+
