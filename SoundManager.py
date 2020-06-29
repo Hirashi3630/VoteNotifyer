ac19b89fec5baa178faec50a072fb0a23dbc7c5b
Hirashi3630 <vladayolo.info@gmail.com>
2020-06-29 16:08:06 +0200

    check if sound file exists

--- SoundManager.py
+++ SoundManager.py
@@ -1,6 +1,13 @@
 from playsound import playsound
+import os
+
+h = __import__('Helper')
 
 
 def Play(file):
     if file != "-1" and file != -1:
-        playsound(file)
+
+        if os.path.isfile(file):
+            playsound(file)
+        else:
+            h.log('Sound path is incorrect', 'red')
