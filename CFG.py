c0d006d7c50f5711b5f8671c4009d9aa5db34920
Hirashi3630 <vladayolo.info@gmail.com>
2020-07-01 12:38:02 +0200

    config system for modular system

--- CFG.py
+++ CFG.py
@@ -1,10 +1,9 @@
+from Helper import Helper
 import json
 import io
 import os
 
 import requests
-
-h = __import__('Helper')
 
 
 class CFG:
@@ -12,21 +11,20 @@ class CFG:
         self.file_path = 'config.json'
 
         data = self.Load()
+        self.data = data
         self.repeat_interval = data['repeat-interval']
         self.scraper_file = data['scraper-file']
         self.scraper_file_par = data['scraper-file-par']
-        self.sound_path = data['sound-path']
-        self.open_browser = data['open-browser']
 
     def Load(self):
         # check if exists - if not create and download default config
         if os.path.isfile(self.file_path) and os.access(self.file_path, os.R_OK):
-            h.log("Config exists and is readable!", 'green')
+            Helper.log("Config exists and is readable!", 'green')
         else:
-            h.log("Either config is missing or is not readable, creating file...", 'yellow')
+            Helper.log("Either config is missing or is not readable, creating file...", 'yellow')
             with io.open(self.file_path, 'w') as db_file:
                 db_file.write(self.GetDefaultConfig())
-            h.log("Edit 'config.json' and execute 'MCNotifyer.py' again.", 'white')
+            Helper.log("Edit 'config.json' and execute 'MCNotifyer.py' again.", 'white')
             input("Press Enter to exit...")
             exit()
 
